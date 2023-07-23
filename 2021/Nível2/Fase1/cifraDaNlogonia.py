dd78d512c1fcbd539c0e7a1b97c41ca9e5b89ee3
sabiss <sabrinabezsilva@gmail.com>
2023-07-23 17:37:11 -0300

    tentando adequando o código para receber palavras com a letra "z"

--- 2021/Nível2/Fase1/cifraDaNlogonia.py
+++ 2021/Nível2/Fase1/cifraDaNlogonia.py
@@ -14,33 +14,41 @@ for letra in palavraParaCifrar:
         letraParaEsquerda = 0
         distanciaDaConsoanteParaDireita = 0
         distanciaDaConsoanteParaEsquerda = 0
-
-        for i in range(posicaoDaLetraNoAlfabeto+1, len(alfabeto), +1):#indo pra direita
-            #vou procurando qual é a próxima vogal
-            distanciaDaConsoanteParaDireita+=1#marca a distância daquela vogal até a consoante original
-            if alfabeto[i] in vogais:
-                letraParaDireita = vogais[vogais.index(alfabeto[i])]
-                break
-
-        for e in range(posicaoDaLetraNoAlfabeto-1, 0, -1):#indo pra esquerda
+        if letra != 'z':
+            for i in range(posicaoDaLetraNoAlfabeto+1, len(alfabeto), +1):#indo pra direita
+                #vou procurando qual é a próxima vogal
+                distanciaDaConsoanteParaDireita+=1#marca a distância daquela vogal até a consoante original
+                if alfabeto[i] in vogais:
+                    letraParaDireita = vogais[vogais.index(alfabeto[i])]
+                    break
+        for e in range(posicaoDaLetraNoAlfabeto-1, -1, -1):#indo pra esquerda
             #for e in range(partida, parada, operação)
             distanciaDaConsoanteParaEsquerda+=1
             if alfabeto[e] in vogais:
                 letraParaEsquerda = vogais[vogais.index(alfabeto[e])]
                 break
-        indexLetraPraDireita = alfabeto.index(letraParaDireita)
-        indexLetraPraEsquerda = alfabeto.index(letraParaEsquerda)
+        if letra != 'z':
+            indexLetraPraDireita = alfabeto.index(letraParaDireita)
+            indexLetraPraEsquerda = alfabeto.index(letraParaEsquerda)
 
-        if distanciaDaConsoanteParaDireita == distanciaDaConsoanteParaEsquerda:
-            novaPalavra+=letraParaEsquerda
-        elif distanciaDaConsoanteParaDireita < distanciaDaConsoanteParaEsquerda:
-            novaPalavra+=letraParaDireita
+            if distanciaDaConsoanteParaDireita == distanciaDaConsoanteParaEsquerda:
+                novaPalavra+=letraParaEsquerda
+            elif distanciaDaConsoanteParaDireita < distanciaDaConsoanteParaEsquerda:
+                novaPalavra+=letraParaDireita
+            else:
+                novaPalavra+=letraParaEsquerda
+            for a in range (posicaoDaLetraNoAlfabeto+1, len(alfabeto), +1):#próxima consoante
+                if palavraParaCifrar[0] == 'z':
+                    novaPalavra+= "z"
+                    break
+                else:
+                    if alfabeto[a] in consoantes:
+                        novaPalavra+=alfabeto[a]
+                        break
         else:
             novaPalavra+=letraParaEsquerda
-        for a in range (posicaoDaLetraNoAlfabeto+1, len(alfabeto), +1):
-            if alfabeto[a] in consoantes:
-                novaPalavra+=alfabeto[a]
-                break
+            novaPalavra+="z"
+        
     else:
         novaPalavra+=letra
 print(novaPalavra)
